97d7fa7f69b29d91a4d0e38a175ec4b82d770de7
Mike Wang <yellowy4321@gmail.com>
2014-02-13 11:58:29 -0800

    Refactored some code on GPSHandler and Main Activity for readibility. Added button on main activity to access the Map Activity for my end. Added MyLocation capabilities removed from previous commits.

--- src/com/csc/bikelaner/GPSHandler.java
+++ src/com/csc/bikelaner/GPSHandler.java
@@ -1,61 +1,92 @@
 package com.csc.bikelaner;
 
+import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 
-public class GPSHandler {
-   /** The interactive Google Map fragment. */
-   private GoogleMap m_vwMap;
-   private final LocationManager locationManager;
-   private final FragmentManager fragmentManager;
+public class GPSHandler implements android.location.LocationListener {
+	/** The interactive Google Map fragment. */
+	private GoogleMap m_vwMap;
+	private final LocationManager locationManager;
+	private final FragmentManager fragmentManager;
 
-   public static final int DEFAULT_ZOOM_LEVEL = 17;
+	public static final int DEFAULT_ZOOM_LEVEL = 17;
 
-   public GPSHandler(LocationManager locationManager, FragmentManager fragmentManager) {
-      this.locationManager = locationManager;
-      this.fragmentManager = fragmentManager;
-      initMapLayout();
-      initMapSettings();
-      setCameraToMyLocation();
-   }
+	public GPSHandler(LocationManager locationManager, FragmentManager fragmentManager) {
+		this.locationManager = locationManager;
+		this.fragmentManager = fragmentManager;		
+	}
 
-   public void initMapLayout() {
-      // Obtain the support map fragment specified in the XML to get the map
-      // object
-      
-      SupportMapFragment map = (SupportMapFragment)fragmentManager.findFragmentById(R.id.map);
-      m_vwMap = map.getMap();
-   }
+	public void initiate() {
+		initMapLayout();
+		initMapSettings();
+		setCameraToMyLocation();
+	}
 
-   public void initMapSettings() {
-      // Enables in the view the ability to track your location.
-      // Rest is self explanatory.
-      m_vwMap.setMyLocationEnabled(true);    
-      m_vwMap.getUiSettings().setCompassEnabled(true);
-      m_vwMap.getUiSettings().setZoomControlsEnabled(true);
+	public void initMapLayout() {
+		// Obtain the support map fragment specified in the XML to get the map
+		// object
+		SupportMapFragment map = (SupportMapFragment) fragmentManager
+				.findFragmentById(R.id.map);
+		m_vwMap = map.getMap();
+		assert(m_vwMap != null);
+	}
 
-   }
+	public void initMapSettings() {
+		// Enables in the view the ability to track your location.
+		// Rest is self explanatory.
+		m_vwMap.setMyLocationEnabled(true);
+		m_vwMap.getUiSettings().setCompassEnabled(true);
+		m_vwMap.getUiSettings().setZoomControlsEnabled(true);
 
-   public void setCameraToMyLocation() {
-      //Create a location manager to get a service location provider
-      //to find my location.
-      String provider = locationManager.getBestProvider(new Criteria(), true);
-      Location location = locationManager.getLastKnownLocation(provider);
+	}
 
-      //Set the camera to my location
-      if (location != null) {
-         LatLng latLng = new LatLng(location.getLatitude(),
-               location.getLongitude());
+	public void setCameraToMyLocation() {
+		// Create a location manager to get a service location provider
+		// to find my location.
+		String provider = locationManager.getBestProvider(new Criteria(), true);
+		Location location = locationManager.getLastKnownLocation(provider);
 
-         m_vwMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,
-               DEFAULT_ZOOM_LEVEL));
-      }
-   }
+		// Set the camera to my location
+		if (location != null) {
+			LatLng latLng = new LatLng(location.getLatitude(),
+					location.getLongitude());
+
+			m_vwMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,
+					DEFAULT_ZOOM_LEVEL));
+		}
+	}
+
+	@Override
+	public void onProviderDisabled(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+	}
 }
--- src/com/csc/bikelaner/MainActivity.java
+++ src/com/csc/bikelaner/MainActivity.java
@@ -5,6 +5,7 @@ import java.util.Observer;
 
 import android.content.Context;
 import android.hardware.SensorManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
@@ -13,100 +14,118 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 import com.csc.bikelaner.db.LocalDataStore;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class MainActivity extends FragmentActivity implements
-   OnClickListener, Observer {
-   private SensorManager sensorManager;
-   private Button btnStart, btnStop, btnDisplayX, btnDisplayY, btnDisplayZ;
-   private boolean started = false;
-   private AccelerometerListener accelData;
+public class MainActivity extends FragmentActivity implements OnClickListener,
+		Observer {
+	private SensorManager sensorManager;
+	private Button btnStart, btnStop, btnDisplayX, btnDisplayY, btnDisplayZ, btnDispMap;
+	private boolean started = false;
+	private AccelerometerListener accelData;
+	private GPSHandler gpsHandler;
 
-   @Override
-   public void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      // Set map layout as the main view
-      // setContentView(R.layout.map_layout);
+	public void init_Buttons() {
+		// Nichols stuff
+		btnStart = (Button) findViewById(R.id.btnStart);
+		btnStop = (Button) findViewById(R.id.btnStop);
+		btnDisplayX = (Button) findViewById(R.id.btnDisplayX);
+		btnDisplayY = (Button) findViewById(R.id.btnDisplayY);
+		btnDisplayZ = (Button) findViewById(R.id.btnDisplayZ);
+		btnStart.setOnClickListener(this);
+		btnStop.setOnClickListener(this);
+		btnDisplayX.setOnClickListener(this);
+		btnDisplayY.setOnClickListener(this);
+		btnDisplayZ.setOnClickListener(this);
+		btnStart.setEnabled(true);
+		btnStop.setEnabled(false);
+		btnDisplayX.setEnabled(false);
+		btnDisplayY.setEnabled(false);
+		btnDisplayZ.setEnabled(false);
+		btnStart.setText("Start");
+		btnStop.setText("Stop");
+		btnDisplayX.setText("<X>");
+		btnDisplayY.setText("<Y>");
+		btnDisplayZ.setText("<Z>");
 
-      setContentView(R.layout.activity_main);
-      sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		//Just to go to Mike's section
+		btnDispMap = (Button) findViewById(R.id.btnDisplayMap);
+		btnDispMap.setText("DisplayMap");
+		btnDispMap.setEnabled(true);		
+		btnDispMap.setOnClickListener(this);
+	}
 
-      btnStart = (Button) findViewById(R.id.btnStart);
-      btnStop = (Button) findViewById(R.id.btnStop);
-      btnDisplayX = (Button) findViewById(R.id.btnDisplayX);
-      btnDisplayY = (Button) findViewById(R.id.btnDisplayY);
-      btnDisplayZ = (Button) findViewById(R.id.btnDisplayZ);
-      btnStart.setOnClickListener(this);
-      btnStop.setOnClickListener(this);
-      btnDisplayX.setOnClickListener(this);
-      btnDisplayY.setOnClickListener(this);
-      btnDisplayZ.setOnClickListener(this);
-      btnStart.setEnabled(true);
-      btnStop.setEnabled(false);
-      btnDisplayX.setEnabled(false);
-      btnDisplayY.setEnabled(false);
-      btnDisplayZ.setEnabled(false);
-      btnStart.setText("Start");
-      btnStop.setText("Stop");
-      btnDisplayX.setText("<X>");
-      btnDisplayY.setText("<Y>");
-      btnDisplayZ.setText("<Z>");
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// Set map layout as the main view
+		// setContentView(R.layout.map_layout);
 
-      new LocalDataStore(getApplicationContext());
-   }
+		setContentView(R.layout.activity_main);
+		sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-   @Override
-   protected void onResume() {
-      super.onResume();
+		init_Buttons();
+		
+		new LocalDataStore(getApplicationContext());
+	}
 
-   }
+	@Override
+	protected void onResume() {
+		super.onResume();
 
-   @Override
-   protected void onPause() {
-      super.onPause();
-      if (started == true) {
-         accelData.stopListening();
-      }
-   }
+	}
 
-   @Override
-   public void onClick(View v) {
-      switch (v.getId()) {
-      case R.id.btnStart:
-          btnStart.setEnabled(false);
-          btnStop.setEnabled(true);
-          started = true;
-          accelData = new AccelerometerListener(sensorManager, this);
-          
-          
-          
-          break;
-      case R.id.btnStop:
-          btnStart.setEnabled(true);
-          btnStop.setEnabled(false);
-          started = false;
-          accelData.stopListening();
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (started == true) {
+			accelData.stopListening();
+		}
+	}
 
-          break;
-      default:
-          break;
-      }
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btnStart:
+			btnStart.setEnabled(false);
+			btnStop.setEnabled(true);
+			started = true;
+			accelData = new AccelerometerListener(sensorManager, this);
 
-   }
+			break;
+		case R.id.btnStop:
+			btnStart.setEnabled(true);
+			btnStop.setEnabled(false);
+			started = false;
+			accelData.stopListening();
 
-   @Override
-   public boolean onCreateOptionsMenu(Menu menu) {
-      // Inflate the menu; this adds items to the action bar if it is present.
-      getMenuInflater().inflate(R.menu.main, menu);
-      return true;
-   }
+			break;
+		case R.id.btnDisplayMap:
+			setContentView(R.layout.map_layout);
+			gpsHandler = new GPSHandler(
+					(LocationManager) getSystemService(LOCATION_SERVICE),
+					getSupportFragmentManager());
+			gpsHandler.initiate();
+			break;
+		default:
+			break;
+		}
 
-   @Override
-   public void update(Observable observable, Object data) {
-      // TODO Auto-generated method stub
-      AccelData sensorData = (AccelData) data;
-      
-      btnDisplayX.setText(String.valueOf(sensorData.getX()));
-      btnDisplayY.setText(String.valueOf(sensorData.getY()));
-      btnDisplayZ.setText(String.valueOf(sensorData.getZ()));
-   }
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void update(Observable observable, Object data) {
+		// TODO Auto-generated method stub
+		AccelData sensorData = (AccelData) data;
+
+		btnDisplayX.setText(String.valueOf(sensorData.getX()));
+		btnDisplayY.setText(String.valueOf(sensorData.getY()));
+		btnDisplayZ.setText(String.valueOf(sensorData.getZ()));
+	}
 }
